cf9e5949977ecb678d2a0583fc4c72f0a20cb96c
witomin <witomin@mail.ru>
2023-04-21 14:15:32 +0300

    Добавлен метод GetAllDNSAsync

--- DNS.cs
+++ DNS.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Yandex.API360.Models;
@@ -18,8 +19,37 @@ namespace Yandex.API360 {
             var result = await response.Content.ReadFromJsonAsync<DNSList>();
             return result;
         }
-        public async Task<DNSList> GetAllDNSAsync() {
-            throw new NotImplementedException();
+        /// <summary>
+        /// Получить все DNS записи домена
+        /// </summary>
+        /// <param name="domainName">Полное доменное имя. Например example.com. Для кириллических доменов (например домен.рф) используйте кодировку Punycode.</param>
+        /// <returns></returns>
+        public async Task<List<DNSRecord>> GetAllDNSAsync(string domainName) {
+            var result = new List<DNSRecord>();
+            var response = await GetDNSAsync(domainName);
+            //определяем сколько всего записей
+            var TotalRecords = response.total;
+            //пытаемся получить все подразделения в одном запросе
+            response = await GetDNSAsync(domainName, 1, TotalRecords);
+            //Проверяем все ли подразделения получены
+            if (response.perPage == TotalRecords) {
+                result = response.records;
+            }
+            else {
+                //если API отдало не все
+                //сохраняем, то что уже получили
+                result.AddRange(response.records);
+                //определяем кол-во страниц ответа
+                var pages = response.pages;
+                //определяем сколько максимально отдает API
+                var perPageMax = response.perPage;
+                // получаем остальные страницы начиная со 2-й
+                for (long i = 2; i <= pages; i++) {
+                    response = await GetDNSAsync(domainName, i, perPageMax);
+                    result.AddRange(response.records);
+                }
+            }
+            return result;
         }
     }
 }
