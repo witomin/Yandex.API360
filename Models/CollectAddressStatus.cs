dffce82b0401bc12a6575474d4b5a4dd1fdc680a
witomin <witomin@mail.ru>
2023-04-27 15:15:08 +0300

    Добавлены методы SetСollectAddressesAsync GetСollectAddressesAsync

---
+++ Models/CollectAddressStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Yandex.API360.Models {
+    /// <summary>
+    /// Статус автоматического сбора контактов
+    /// </summary>
+    public class CollectAddressStatus {
+        /// <summary>
+        /// Автоматически собирать контакты
+        /// </summary>
+        public bool collectAddresses { get; set; }
+    }
+}
--- PostSettings.cs
+++ PostSettings.cs
@@ -10,7 +10,7 @@ namespace Yandex.API360 {
         /// <summary>
         /// Получить почтовый адрес, с которого отправляются письма по умолчанию, и настройки подписей сотрудника
         /// </summary>
-        /// <param name="userId"></param>
+        /// <param name="userId">Идентификатор пользователя</param>
         /// <returns></returns>
         public async Task<Signatures> GetUserSignaturesAsync(ulong userId) {
             var response = await httpClient.GetAsync($"{_options.URLPostSettings}/users/{userId}/settings/sender_info");
@@ -20,13 +20,36 @@ namespace Yandex.API360 {
         /// <summary>
         /// Установить почтовый адрес сотрудника, с которого отправляются письма по умолчанию, и настройки его подписей
         /// </summary>
-        /// <param name="userId"></param>
+        /// <param name="userId">Идентификатор пользователя</param>
         /// <param name="signatures"></param>
         /// <returns></returns>
         public async Task<Signatures> SetUserSignaturesAsync(ulong userId, Signatures signatures) {
             var response = await httpClient.PostAsJsonAsync($"{_options.URLPostSettings}/users/{userId}/settings/sender_info", signatures);
             await CheckResponseAsync(response);
             return await response.Content.ReadFromJsonAsync<Signatures>();
+        }
+        /// <summary>
+        /// Получить статус автоматического сбора контактов
+        /// Метод позволяет просмотреть, включено ли автоматическое формирование адресной книги сотрудника из адресов исходящей почты.
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <returns></returns>
+        public async Task<CollectAddressStatus> GetСollectAddressesAsync(ulong userId) {
+            var response = await httpClient.GetAsync($"{_options.URLPostSettings}/users/{userId}/settings/address_book");
+            await CheckResponseAsync(response);
+            return await response.Content.ReadFromJsonAsync<CollectAddressStatus>();
+        }
+        /// <summary>
+        ///  Установить статус автоматического сбора контактов
+        ///  Метод позволяет управлять опцией автоматического формирования адресной книги сотрудника из адресов исходящей почты.
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <param name="collectAddresses">Автоматически собирать контакты</param>
+        /// <returns></returns>
+        public async Task<CollectAddressStatus> SetСollectAddressesAsync(ulong userId, CollectAddressStatus collectAddresses) {
+            var response = await httpClient.PostAsJsonAsync($"{_options.URLPostSettings}/users/{userId}/settings/address_book", collectAddresses);
+            await CheckResponseAsync(response);
+            return await response.Content.ReadFromJsonAsync<CollectAddressStatus>();
         }
     }
 }
