5858dbc4a5f04655c85a3ea8b912064a20139086
Виталий Тюрин <vitaly.tyurin@niap.novomoskovsk.ru>
2024-11-13 14:14:31 +0300

    fix

--- Models/Mailbox/ResourceType.cs
+++ Models/Mailbox/ResourceType.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Yandex.API360.Models.Mailbox {
+    [JsonConverter(typeof(JsonStringEnumMemberConverter))]
     public enum ResourceType {
         /// <summary>
         /// Общий
